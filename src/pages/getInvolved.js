2602d8385c86ab18e109d0d255cbb4a1e935aed8
Dhanjeet09 <dhanjeetsharma7@gmail.com>
2024-10-03 13:00:43 +0530

    update ourLegacy.js and getInvolved.js

--- src/pages/getInvolved.js
+++ src/pages/getInvolved.js
@@ -4,11 +4,11 @@ import SignUp from "@/components/ui/SignUp";
 export default function GetInvolved() {
   return (
     <>
-      <div id="volunteer">
-        <SignUp />
-      </div>
       <div id="donate">
         <Donate />
+      </div>
+      <div id="volunteer">
+        <SignUp />
       </div>
     </>
   );
