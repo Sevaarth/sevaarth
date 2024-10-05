a2f5cf894b3954dfd2959e0a98414632005d7ea6
Mohit Mehto <imohitmehto@gmail.com>
2024-10-05 19:42:57 +0530

    fix navbar issue and update donation api and add side scroll

---
+++ src/pages/donate.js
@@ -0,0 +1,10 @@
+const Donate = () => {
+  return (
+    <div className="bg-red-500 min-h-screen">
+      <h1 className="text-white text-4xl text-center py-20">Donation Page</h1>
+      {/* Other content here */}
+    </div>
+  );
+};
+
+export default Donate;
